2b40ff3b83c3817968aff7eb081ce365406d1550
Hunter Hodnett <hchodnett@gmail.com>
2023-09-08 17:45:39 -0400

    update return URL

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -34,7 +34,7 @@ export default async function handler(
       const paymentURL = await user.getPaymentURL({
         // Return the user to the homepage after they've paid
         // BASE_URL is set in .env to be the URL of the homepage
-        returnToUrl: "http://localhost:3001",
+        returnToUrl: process.env.AUTH0_BASE_URL,
       });
       res.status(200).json({
         content: `You don't have enough credits to send a message. Please add more credits at ${paymentURL}`,
