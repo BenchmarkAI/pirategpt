671ba30a24beca41b9c951f9e6fc7dc55db84312
Hunter Hodnett <hchodnett@gmail.com>
2023-10-12 16:55:05 -0400

    fix typo

--- pages/api/credits/[...chipp].ts
+++ pages/api/credits/[...chipp].ts
@@ -1,7 +1,7 @@
 import { handleCredits } from "@chipp/nextjs-chipp";
 import mockUserId from "@/utils/getMockUserId";
 
-// We use mockUserId nly for demonstration purposes.
+// We use mockUserId only for demonstration purposes.
 // You will need to retrieve a unique identifier for
 // your user via the HTTP request. E.g. pass a cookie
 // to an auth library and use the user's email as the
